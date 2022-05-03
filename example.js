506c1217d6d172f8a8ce8564b543403beb530b30
AnderHex <103506080+anderhex@users.noreply.github.com>
2022-05-03 17:45:20 -0300

    Adicionada condicional que verifica a existência de uma 'LIB_URL'

--- example.js
+++ example.js
@@ -3,22 +3,13 @@ import zempt from './zempt.js';
 // Setting the file extensions
 // Default: EPUB, MOBI and PDF. 
 
-//zempt.FILE_EXTENSIONS = ['epub'];
+zempt.FILE_EXTENSIONS = ['epub'];
+zempt.LIB_URL = "https://pt.br1lib.org";
 
 // Searching for a book by book title
 // its returns a list of objects contains the book title,
 // publisher, year, file-format and url. 
-//let books = await zempt.searchBook('As Crônicas de Nárnia');
+let books = await zempt.searchBook('As Crônicas de Nárnia');
 
 // Showing the list
-//console.log(books);
-
-// You can get the direct link to download the book using
-// this function. 
-//let firstBook   = books[0];
-//let downloadUrl = await zempt.getDownloadLink(firstBook);
-//
-// Showing the result
-console.log(arguments);
-
-// Enjoy! :)
+console.log(books);
